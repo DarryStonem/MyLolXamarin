ff81d258bd2c6eb76ab7b44466cf4513bd5ca552
Cristian González <cristian@darrystonem.com>
2019-07-20 17:23:13 -0500

    Added Repleace file

---
+++ Source/MyLoL.Android/appcenter-pre-build.sh
@@ -0,0 +1,12 @@
+# Updating ids for replacement
+
+IdFile=$BUILD_REPOSITORY_LOCALPATH/SourceCode/ProField/Helpers/Constants.cs
+
+sed -i '' "s#LOLAPIKEY#$LOLAPIKEY#g" $IdFile
+sed -i '' "s#APPCENTERIOS#$APPCENTERIOS#g" $IdFile
+sed -i '' "s#APPCENTERANDROID#$APPCENTERANDROID#g" $IdFile
+
+# Print out file for reference
+cat $IdFile
+
+echo "Updated id!"
