93381d575ce02750b8a6d8dd28fc964afc773867
Alex González <alex@darry.dev>
2019-11-30 17:08:28 -0600

    [Feature] - Fixed Mobile App

--- Source/MyLoL/Constants.cs
+++ Source/MyLoL/Constants.cs
@@ -3,8 +3,8 @@ namespace MyLoL
 {
     public static class Constants
     {
-        public static string APIKEY = "RGAPI-8be58374-a9e8-42ef-9140-0e99bd326437";
-        public static string AppCenteriOS = "b6c11ce4-8ea1-4601-961a-38b36a5caea6";
-        public static string AppCenterAndroid = "9cb5b161-e62a-425a-997a-57de2493ff00";
+        public static string APIKEY = "LOLAPIKEY";
+        public static string AppCenteriOS = "APPCENTERIOS";
+        public static string AppCenterAndroid = "APPCENTERANDROID";
     }
 }
